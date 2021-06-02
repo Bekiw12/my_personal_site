d3f33e6d4b0cd8e0ade073f6abe9a69332acc100
Bekzod <bekiwnamazov12@gmail.com>
2021-06-02 20:56:18 +0500

    1

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import BlogUser, Skills, About, Accomplishments
 
-admin.site.register(User)
+admin.site.register(BlogUser)
 admin.site.register(Skills)
 admin.site.register(About)
 admin.site.register(Accomplishments)
