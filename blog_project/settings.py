9300f539b533b7ba96978ce703c6a4791a18605b
Bekzod <bekiwnamazov12@gmail.com>
2021-06-07 19:53:48 +0500

    settings

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -135,7 +135,7 @@ EMAIL_USE_SSL = True
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '/static/'),
 )
-
+#
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
